f736dcdd6745d4ecd006c1e25d990a30ac64f61c
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2022-08-01 12:49:03 +0500

    MVVM Architecture

--- lib/data/app_exceptions.dart
+++ lib/data/app_exceptions.dart
@@ -1,4 +1,4 @@
-class AppExceptions {
+abstract class AppExceptions {
   final _message;
   final _prefix;
 
@@ -8,3 +8,23 @@ class AppExceptions {
     return '${_prefix + _message}';
   }
 }
+
+class FetchDataException extends AppExceptions{
+  FetchDataException([String? message]):super(message, 'Error Loading Data ');
+}
+
+
+class BadRequestExceptions extends AppExceptions{
+  BadRequestExceptions([String? message]) :super(message, 'Bad Request');
+}
+
+
+class UnathorizedExceptions extends AppExceptions{
+  UnathorizedExceptions([String? message]):super(message, 'Invalid Token');
+}
+
+
+class InvalidInputExceptions extends AppExceptions{
+  InvalidInputExceptions(String? message):super(message, 'Invalid Input ');
+}
+
